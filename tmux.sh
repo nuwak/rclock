d134d989c91e1d67b1991abeeea080b0ab711e02
Nuwak <nuwak.dev@pm.me>
2024-05-26 20:06:01 +0200

    tmux

--- tmux.sh
+++ tmux.sh
@@ -13,7 +13,7 @@ if [ $? != 0 ]; then
   tmux split-window -h -t $SESSION_NAME:0.2
 
   tmux send-keys -t $SESSION_NAME:0.0 'rclock -D' C-m
-  tmux send-keys -t $SESSION_NAME:0.1 "rclock -d 8h" C-m
+  tmux send-keys -t $SESSION_NAME:0.1 "rclock -d '0:0:10' -x 'paplay files/notification.wav'" C-m
   tmux send-keys -t $SESSION_NAME:0.3 "rclock -c 3 -t Asia/Manila" C-m
   tmux select-pane -t $SESSION_NAME:0.2
   tmux send-keys -t $SESSION_NAME:0.2 'rclock' C-m
